a90a3c7758519f7880e497749078936df822e680
kenskiyyy <55867165+kenskiyyy@users.noreply.github.com>
2023-01-27 22:08:34 +0300

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Do argument checks
 if [ ! "$#" -ge 1 ]; then
     echo "Usage: $0 {size}"
     echo "Example: $0 4G"
@@ -9,31 +8,19 @@ if [ ! "$#" -ge 1 ]; then
     exit 1
 fi
 
-
-## Intro
-echo "Root access is required, please run as root or enter sudo password." 
+echo "Enter root password." 
 echo
-
-## Setup variables
-
-# Get size from first argument
 SWAP_SIZE=$1
-
-# Get path from second argument (default to /swapfile)
 SWAP_PATH="/swapfile"
+
 if [ ! -z "$2" ]; then
     SWAP_PATH=$2
 fi
 
-
-## Run
-sudo fallocate -l $SWAP_SIZE $SWAP_PATH  # Allocate size
-sudo chmod 600 $SWAP_PATH                # Set proper permission
-sudo mkswap $SWAP_PATH                   # Setup swap         
-sudo swapon $SWAP_PATH                   # Enable swap
-echo "$SWAP_PATH   none    swap    sw    0   0" | sudo tee /etc/fstab -a # Add to fstab
-
-## Outro
-
+sudo fallocate -l $SWAP_SIZE $SWAP_PATH  
+sudo chmod 600 $SWAP_PATH                
+sudo mkswap $SWAP_PATH                            
+sudo swapon $SWAP_PATH                   
+echo "$SWAP_PATH   none    swap    sw    0   0" | sudo tee /etc/fstab -a 
 echo
-echo "Done! You now have a $SWAP_SIZE swap file at $SWAP_PATH"
+echo "Success. $SWAP_SIZE swap file enabled at $SWAP_PATH"
